e76bf7cd385d807090a2e9078a58641be279919f
nica18morena <jessica.frierson@live.com>
2017-02-05 15:10:38 -0800

    Parenting plan setup up untill weekend drop off time. Need to add the other sections.

--- app/src/main/java/com/example/jessi/guardiano/ParentingPlanSetup.java
+++ app/src/main/java/com/example/jessi/guardiano/ParentingPlanSetup.java
@@ -1,13 +1,131 @@
 package com.example.jessi.guardiano;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class ParentingPlanSetup extends AppCompatActivity {
+public class ParentingPlanSetup extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
+    private TextView weekendDropOffTimeText, weekendPickUpTimeText;
+    private TimePicker timePicker;
+    private Button weekendDropoffTimeButton, weekendPickUpTimeButton;
+    private int hour, minute;
+    static final int TIME_DIALOG_ID = 999;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parenting_plan_setup);
+
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_weekend_options);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Every");
+        categories.add("Every other");
+        categories.add("First wknd of mos");
+        categories.add("Second wknd of mos");
+        categories.add("Third wknd of mos");
+        categories.add("Fourth wknd of mos");
+        categories.add("Fifth wknd of mos");
+        categories.add("None");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+        //call date time pickers
+        //addListeneronButton();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    /*public void addListeneronButton(){
+
+        weekendDropoffTimeButton = (Button) findViewById(R.id.button_drop_off);
+
+        weekendDropoffTimeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                showDialog(TIME_DIALOG_ID);
+
+            }
+
+        });
+    }*/
+    /*@Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case TIME_DIALOG_ID:
+                // set time picker as current time
+                return new TimePickerDialog(this,
+                        timePickerListener, hour, minute,false);
+
+        }
+        return null;
+    }*/
+
+    /*private TimePickerDialog.OnTimeSetListener timePickerListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int selectedHour,
+                                      int selectedMinute) {
+                    hour = selectedHour;
+                    minute = selectedMinute;
+
+                    // set current time into textview
+                    weekendDropOffTimeText.setText(new StringBuilder().append(pad(hour))
+                            .append(":").append(pad(minute)));
+
+                    // set current time into timepicker
+                    timePicker.setCurrentHour(hour);
+                    timePicker.setCurrentMinute(minute);
+
+                }
+            };*/
+    private static String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
     }
 }
