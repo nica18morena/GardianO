1036701e4931a8f93bfb09fecd54d433b6fd0cba
nica18morena <jessica.frierson@live.com>
2017-02-05 18:02:24 -0800

    Updates drop off time after choosing the picker time.

--- app/src/main/java/com/example/jessi/guardiano/TimePickerFragment.java
+++ app/src/main/java/com/example/jessi/guardiano/TimePickerFragment.java
@@ -6,6 +6,7 @@ import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.text.format.DateFormat;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
@@ -17,6 +18,7 @@ import java.util.Calendar;
 public class TimePickerFragment extends DialogFragment
                                     implements TimePickerDialog.OnTimeSetListener{
     private int pickerHour, pickerMin;
+    private String am_pm;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current time as the default values for the picker
@@ -31,7 +33,13 @@ public class TimePickerFragment extends DialogFragment
 
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         // Do something with the time chosen by the user
-        pickerHour = hourOfDay;
+
+        am_pm = hourOfDay > 12? "PM" : "AM";
+
+        pickerHour = hourOfDay > 12? hourOfDay - 12 : hourOfDay;
         pickerMin = minute;
+
+        TextView tv = (TextView) getActivity().findViewById(R.id.text_drop_off);
+        tv.setText(String.valueOf(hourOfDay) +":" + String.valueOf(minute) + " "+ am_pm);
     }
 }
